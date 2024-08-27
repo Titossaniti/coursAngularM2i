438c91ab2ec3c60aeeb973798ec9b20923bf8223
Tristan Erard <tristan.erard@gmail.com>
2024-08-27 16:57:42 +0200

    Issue with subscribe & httpModule

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -4,6 +4,8 @@ import {UserDetailComponent} from "../user-detail/user-detail.component";
 import {NgForOf} from "@angular/common";
 import {ButtonDeleteComponent} from "../button-delete/button-delete.component";
 import type {User} from "../user.type";
+import {HttpClient} from "@angular/common/http";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-user-list',
@@ -14,33 +16,51 @@ import type {User} from "../user.type";
 })
 export class UserListComponent implements OnInit {
 
+  constructor(private http: HttpClient) {}
+  url = "https://jsonplaceholder.typicode.com/users";
+
   searchText: string = "";
 
   users  :User[] = [];
+  filteredUsers : User[] = [];
 
   deleteUser(id:number){
     this.users = this.users.filter(user => user.id !== id);
   }
 
+  // onSearchChange(searchText:string){
+  //   console.log("USER_LIST : ",searchText);
+  //   this.searchText=searchText;
+  // }
+
+  ngOnInit():void {
+    this.http.get<User[]>(this.url).subscribe(
+    (data) => {
+      this.users = data
+    },
+    (error) => {
+      console.log(error)
+    });
+  }
+
   onSearchChange(searchText:string){
-    console.log("USER LIST : ",searchText);
-    this.searchText=searchText;
+    this.filteredUsers = this.users.filter(user => user.name !== searchText);
   }
 
-  async ngOnInit(): Promise<void> {
-    try {
-      const url = "https://jsonplaceholder.typicode.com/users"
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      this.users = await response.json();
 
-    } catch (error) {
-      console.log(error);
-    }
-
-  }
+  // async ngOnInit(): Promise<void> {
+  //   try {
+  //     const response = await fetch(this.url);
+  //     if (!response.ok) {
+  //       throw new Error(response.statusText);
+  //     }
+  //     this.users = await response.json();
+  //
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  //
+  // }
 
   condition:string="B";
 }
