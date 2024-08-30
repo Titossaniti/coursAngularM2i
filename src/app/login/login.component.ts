8f1988fe28208d21969b4ff14658bfccdfa17e04
Titossaniti <tristan.erard@gmail.com>
2024-08-30 10:09:29 +0200

    Form for login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, CommonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  user = {
+    username:'',
+    password:'',
+  }
+
   constructor(private router: Router) {}
 
-  goNavigate() {
-    this.router.navigate(['']);
+  login() :void {
+    console.log(this.user);
   }
 }
